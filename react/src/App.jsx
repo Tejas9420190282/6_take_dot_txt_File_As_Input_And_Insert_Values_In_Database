44893e2d295c577c231924f64a18defddce8d906
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-07-21 08:42:27 +0530

    Admin add teacher

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Login from "./component/Login";
 import Teacher_Home from "./component/teacher/Teacher_Home";
 import Admin_Home from "./component/admin/Admin_Home";
+import Admin_Add_Teacher from "./component/admin/Admin_Add_Teacher";
 
 
 function App() {
@@ -21,6 +22,15 @@ function App() {
                 </>
             ),
         },
+        {
+            path: "/admin/add-teacher",
+            element: (
+                <>
+                    <Admin_Add_Teacher />
+                </>
+            ),
+        },
+
         {
             path: "/teacher/home",
             element: <Teacher_Home />,
--- react/src/component/admin/Admin_Add_Teacher.jsx
+++ react/src/component/admin/Admin_Add_Teacher.jsx
@@ -1,33 +1,35 @@
 
-// Login.jsx
-
 import React, { useState } from "react";
 import axios from 'axios';
 import { Link, useNavigate } from "react-router-dom";
 
-function Login() {
+function Admin_Add_Teacher() {
     
     const [email, setEmail] = useState("");
-
     const [password, setPassword] = useState("");
-    
+    const [name, setName] = useState("");
+
     const [msg, setMsg] = useState("");
+
+    console.log("msg : ", msg);
+    
 
     const navigate = useNavigate(); 
 
-    const handleLoginSubmit = async (e) => {
+    const handleRegisterSubmit = async (e) => {
+        
         e.preventDefault();
 
-        const responce = await axios.post("http://localhost:7878/login", {
-            email, password
+        const responce = await axios.post("http://localhost:7878/register-teacher", {
+            name, email, password
         });
 
-        if (responce.data.success) {
+        if (responce.data.message) {
+            
+            setMsg(responce.data.message);
+        }
 
-            if (responce.data.redirect) {
-                
-                navigate(responce.data.redirect);
-            }
+        if (responce.data.success) {
             
             if (responce.data.message) {
                 
@@ -35,13 +37,22 @@ function Login() {
             }
         }
     };
-
+    
     return (
         <>
             <form
-                onSubmit={handleLoginSubmit}
+                onSubmit={handleRegisterSubmit}
                 className="flex justify-center items-center flex-col gap-3"
             >
+                <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="bg-black px-3 py-2 text-white "
+                    placeholder="Enter your Name"
+                />
+
                 <input
                     type="email"
                     required
@@ -66,13 +77,13 @@ function Login() {
                 >
                     Register Teacher
                 </button>
-
-                <p className="text-red-500 text-3xl">{msg}</p>
             </form>
+
+            <p className="bg-red-500 text-2xl mt-10 ">{msg}</p>
         </>
     )
 }
 
-export default Login;
+export default Admin_Add_Teacher;
 
 
